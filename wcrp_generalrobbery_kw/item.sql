b766b6afee602e66df505d52b31de56a545fcca5
KingWalnut <50270826+KingWalnut@users.noreply.github.com>
2020-12-06 03:07:22 -0800

    Update item.sql

--- wcrp_generalrobbery_kw/item.sql
+++ wcrp_generalrobbery_kw/item.sql
@@ -1,4 +1,6 @@
 USE `vorp`;
 
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('lockpick', 'Lockpick', 60, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('stolenmerch', 'Vendas', 60, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('coins', 'coins', 60, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oldwatch', 'oldwatch', 60, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oldbuckle', 'oldbuckle', 60, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('fan', 'fan', 60, 1, 'item_standard', 0);
